950d0ee21e2fbfc4a968bd3ea13b1e0f92f84360
Ben Price <price.benjamindaniel@gmail.com>
2017-03-03 22:44:12 -0800

    Rewrote source files
    Parsing of command-line arguments in main() was entirely redone. This
    program now accepts -u, --upper-bound, -l, and --lower-bound as
    arguments. These flags only change the data that is written to the CSV
    file. Additionally, there is some very simple (a.k.a. unverified) data
    processing that computes average % transmittance if a multiple of three
    SPA files were passed to the program.

---
+++ BUILD/Source/read-write.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Ensure that ub > lb, ub <= MAX_WAVENUMBER, lb >= MIN_WAVENUMBER, etc...
+void checkBounds(int* upperBound, int* lowerBound, int max, int min)
+{
+	if(*upperBound == *lowerBound) // Dereference and compare
+	{
+		cerr << "Error: checkBounds(int,int): upperbound equals lowerbound." << endl;
+		exit(1);
+	} else if(*upperBound < *lowerBound) {// Swap value stored in upperbound with value in lowerbound
+		int temp = *upperBound;
+		*upperBound = *lowerBound;
+		*lowerBound = temp;
+	}
+	// Once swapped, check for invalid bounds
+	if(*upperBound > max && *lowerBound < min) {
+		cerr << "Error: checkBounds(int,int): upperbound > MAX_WAVENUMBER and lowerbound < MIN_WAVENUMBER." << endl;
+		exit(1);
+	} else if(*upperBound > max) {
+		cerr << "Error: checkBounds(int,int): upperbound > MAX_WAVENUMBER." << endl;
+		exit(1);
+	} else if(*lowerBound < min) {
+		cerr << "Error: checkBounds(int,int): lowerbound < MIN_WAVENUMBER." << endl;
+		exit(1);
+	}
+}
+
+void checkIfNull(void* pointer, const char* callingFunc, const char* ptrDef)
+{
+	if(pointer == nullptr)
+	{
+		cerr << "Error: " << callingFunc << ": unable to allocate memory for " << ptrDef << ".\n";
+		exit(1);
+	}
+	return;
+}
+
+// Verify that the SPA file can be opened
+bool canOpenFile(char* FILENAME)
+{
+	ifstream file (FILENAME, ios::in|ios::binary);
+	bool ableToOpen = file.is_open();
+	if(ableToOpen) file.close();
+	return ableToOpen;
+}
+
+// Read the contents of the SPA file into an array
+void readSPAFile(char* SPA_FILENAME, float IR_Data[], int length, int start)
+{
+    if(canOpenFile(SPA_FILENAME))
+    {
+	    ifstream spaInputFile (SPA_FILENAME, ios::in|ios::binary);
+	    // Go to first datum, then read data into IR_Data
+	    spaInputFile.seekg(start, ios::beg);
+	    spaInputFile.read(reinterpret_cast<char*>(IR_Data), length*4); // 4 bytes per datum
+	    // We must reinterpret IR_Data as a character pointer in order to correctly read in the data
+	    spaInputFile.close();
+	} else {
+		cerr << "Error: Unable to open " << SPA_FILENAME << "." << endl;
+		exit(1);
+	}
+	return;
+}
+
+// Append str to SPA_FILENAME
+// Made const in order to fix "invalid conversion from 'const char*' to 'char*' [-fpermissive]" error
+const char* appendStr(char* SPA_FILENAME, const char* str)
+{
+	string spaString = SPA_FILENAME;
+	return spaString.append(str).c_str();
+}
+
+// Convert a wavenumber to nearest index
+int wavenumToIndex(int wavenumber, float wavenumberArray[], int size)
+{
+	//cout << "wavenumToIndex(int): passed argument 'wavenumber' = " << wavenumber << "." << endl;
+	int candidateIndex = 0;
+    // Find the value in wavenumberArray that is closest to wavenumber
+    for(int i = 0; i < size; i++)
+    {
+    	if(wavenumber < wavenumberArray[i]) 
+    		candidateIndex = i;
+    	else 
+    		break;
+    } // candidateIndex now stores the index of the wavenumberArray value which is JUST GREATER THAN wavenumber
+    float leftDistance = abs(wavenumber - wavenumberArray[candidateIndex]);
+    float rightDistance = abs(wavenumber - wavenumberArray[candidateIndex + 1]);
+    if(leftDistance == rightDistance || leftDistance < rightDistance)
+    {
+    	//cout << "Found candidateIndex: " << candidateIndex << " with corresponding value " << wavenumberArray[candidateIndex] << "." << endl;
+    	return candidateIndex;
+    }
+    else
+    {
+    	//cout << "Found candidateIndex: " << candidateIndex + 1 << " with corresponding value " << wavenumberArray[candidateIndex + 1] << "." << endl;
+    	return candidateIndex + 1; 	// Note: candidateIndex cannot ever be the last index;
+    								// this is prevented by truncation and checkBounds()
+    }
+}
+
+// Print array to CSV file
+void printToCSV(const char* CSV_FILENAME, float IR_Data[], float wavenumber[], int length)
+{
+    ofstream csvOutputFile (CSV_FILENAME, ios::out);
+    if(csvOutputFile.is_open()){
+	    for(int a = 0; a < length; a++) {
+	        csvOutputFile << wavenumber[a] << ", " << IR_Data[a] << endl;
+	    }
+	    csvOutputFile.close();
+	} else {
+		cerr << "Error: printToCSV(const char*, float[], float[], int): Unable to open output file " << CSV_FILENAME << "." << endl;
+		exit(1);
+	}
+	return;
+}
+
+void printToCSV(const char* CSV_FILENAME, float IR_Data[], float wavenumber[], int length, int upperBound, int lowerBound)
+{
+	int lowerIndex = wavenumToIndex(upperBound, wavenumber, length); // Woops...that's confusing
+	int upperIndex = wavenumToIndex(lowerBound, wavenumber, length); // Higher wavenumbers have lower indices! That's how the data is ordered.
+    ofstream csvOutputFile (CSV_FILENAME, ios::out);
+    if(csvOutputFile.is_open()){
+	    for(int a = lowerIndex; a < upperIndex + 1; a++) {
+	        csvOutputFile << wavenumber[a] << ", " << IR_Data[a] << endl;
+	    }
+	    csvOutputFile.close();
+	} else {
+		cerr << "Error: printToCSV(const char*, float[], float[], int, int, int): Unable to open output file " << CSV_FILENAME << "." << endl;
+		exit(1);
+	}
+	return;
+}
+
+void printToCSV
+(
+	const char* CSV_FILENAME,
+	char** SPA_FILENAME,
+	float** IR_Data, 
+	float wavenumber[], 
+	int NUM_SPA_FILES, 
+	int length, 
+	int upperBound, 
+	int lowerBound
+)
+{
+	int lowerIndex = wavenumToIndex(upperBound, wavenumber, length);
+	int upperIndex = wavenumToIndex(lowerBound, wavenumber, length);
+	ofstream csvOutputFile (CSV_FILENAME, ios::out);
+	if(csvOutputFile.is_open())
+	{
+		// Headings
+		csvOutputFile << "Wavenumber, ";
+		for(int i = 0; i < NUM_SPA_FILES - 1; i++)
+			csvOutputFile << SPA_FILENAME[i] << ", ";
+		csvOutputFile << SPA_FILENAME[NUM_SPA_FILES - 1] << endl;
+
+		// Data
+		for(int i = lowerIndex; i < upperIndex + 1; i++)
+		{
+			csvOutputFile << wavenumber[i] << ", ";
+			for(int j = 0; j < NUM_SPA_FILES - 1; j++)
+				csvOutputFile << IR_Data[j][i] << ", ";
+			csvOutputFile << IR_Data[NUM_SPA_FILES - 1][i] << endl;
+		}
+		csvOutputFile.close();
+	}
+	else
+	{
+		cerr << "Error: printToCSV(const char*, char**, float**, float[], int, int, int, int): unable to open output file." << endl;
+		exit(1);
+	}
+	return;
+}
---
+++ BUILD/Source/strToInt.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+// Convert characters '0','1',...,'9' to their integer value
+int charToInt(char a)
+{
+	int integer = 0;
+	switch(a)
+	{
+		case '0': break; // integer is defined as 0
+		case '1': integer = 1; break;
+		case '2': integer = 2; break;
+		case '3': integer = 3; break;
+		case '4': integer = 4; break;
+		case '5': integer = 5; break;
+		case '6': integer = 6; break;
+		case '7': integer = 7; break;
+		case '8': integer = 8; break;
+		case '9': integer = 9; break;
+		default: {
+			cerr << "Error: charToInt(char): argument was not a decimal integer." << endl;
+			exit(1);
+		}
+	}
+	return integer;
+}
+
+//
+string truncateStrAt(string str, char truncAt)
+{
+	int length = str.length();
+	for(int i = 0; i < length; i++)
+		if(str[i] == truncAt) return str.substr(0,i);
+	// If we're here, truncAt was not found in string
+	return str;
+}
+
+//
+int strToInt(string numberAsString)
+{
+	int integer = 0;
+	// Check for decimal point and truncate
+	char decimalPoint = '.';
+	numberAsString = truncateStrAt(numberAsString, decimalPoint);
+	int length = numberAsString.length();
+	// Check for negative
+	if(numberAsString[0] == '-')
+	{
+		string absVal = numberAsString.substr(1); // Read from index 1 to the end
+		return -1 * strToInt(absVal);
+	} else {
+		int power = 0;
+		for(int i = length - 1; i >= 0; i--) // Start at the end of the string and read backwards
+		{
+			integer += charToInt(numberAsString[i]) * pow(10, power);
+			power++;
+		}
+	}
+	return integer;
+}
