5b70c6f87c86b05eb1dd392acff3c3076869f01b
Ben Price <price.benjamindaniel@gmail.com>
2017-03-06 15:43:53 -0800

    Updated header.h to include all necessary functions

--- BUILD/Source/header.h
+++ BUILD/Source/header.h
@@ -10,9 +10,37 @@
 using namespace std;
 
 void printUsage(char* PROG_NAME);
+
+// parse-command-line-args.cpp
 bool usingOptionalArgs(int argc, char** argv, const int numOptionalArgs, bool* optionalArgs[], int optionalArgIndices[]);
+int checkArgOrder(int NUM_OPT_ARGS, bool* optionalArgs[], int optionalArgIndices[], int argc, char* argv[]);
+// str-to-int.cpp
 string truncateStrAt(string str, char truncAt);
 string getStrAfter(string str, char dividingChar);
-void checkArgOrder(int NUM_OPT_ARGS, bool* optionalArgs[], int optionalArgIndices[], int argc, char* argv[]);
+int strToInt(string numberAsString);
+// read-write.cpp
+void printToCSV
+(
+	const char* CSV_FILENAME,
+	char** SPA_FILENAME,
+	float** IR_Data, 
+	float wavenumber[], 
+	int NUM_SPA_FILES, 
+	int length, 
+	int upperBound, 
+	int lowerBound
+);
+void readSPAFile(char* SPA_FILENAME, float IR_Data[], int length, int start);
+const char* createCSVFilename(const char* filename, string ubStr, string lbStr);
+// create-array.cpp
+char** createSPAFileArray(int NUM_SPA_FILES, int numOptArgs, char* argv[], const char* ptrDef);
+float** createFloatArray(int numCols, int numRows, const char* ptrDef);
+char** createAvgDataColTitles(int numGroups, int groupSize, char** SPA_FILENAME, const char* ptrDef);
+// data-processing.cpp
+void checkBound(int* upperBound, int* lowerBound, int max, int min);
+void checkBound(int bound, int MAX_OR_MIN_WAVENUM, bool isUpperBound);
+int wavenumToIndex(int wavenumber, float wavenumberArray[], int size);
+void computeAverages(float** AVG_DATA, float** IR_DATA, int numGroups, int groupSize, int SIZE);
+void computeConstCorr(float** CORR_DATA, float** IR_DATA, int NUM_SPA_FILES, float WAVENUMBER[], int SIZE, int ubCorr, int lbCorr);
 
 #endif
